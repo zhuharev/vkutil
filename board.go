e381e0f5087b5be88d70ad0501c1650326546583
zhuharev <unybigk@gmail.com>
2020-03-15 13:33:06 +0300

    Add get board topic method

--- board.go
+++ board.go
@@ -1,11 +1,16 @@
 package vkutil
 
 import (
+	"context"
 	"encoding/json"
 	"log"
 	"net/url"
 
 	"github.com/zhuharev/vk"
+)
+
+const (
+	MethodBoardGetTopics = "board.getTopics"
 )
 
 // Topic board item
@@ -47,4 +52,43 @@ func (api *Api) BoardGetCommetns(groupID, topicID int, args ...url.Values) ([]Co
 		return nil, err
 	}
 	return r.Response.Items, nil
+}
+
+func (api *Api) BoardGetAllTopics(ctx context.Context, groupID int) ([]Topic, error) {
+	var (
+		count  = 100
+		offset = 0
+		result []Topic
+	)
+	for {
+		topics, err := api.BoardGetTopics(ctx, groupID, count, offset)
+		if err != nil {
+			return nil, err
+		}
+		offset += len(topics)
+		result = append(result, topics...)
+
+		if len(topics) < count {
+			break
+		}
+	}
+	return result, nil
+}
+
+func (api *Api) BoardGetTopics(ctx context.Context, groupID int, count int, offset int) ([]Topic, error) {
+	params := setToUrlValues("group_id", groupID)
+	params = setToUrlValues("count", count, params)
+	params = setToUrlValues("offset", offset, params)
+
+	bts, err := api.VkApi.Request(MethodBoardGetTopics, params)
+	if err != nil {
+		return nil, err
+	}
+
+	var r ResponseTopics
+	err = json.Unmarshal(bts, &r)
+	if err != nil {
+		return nil, err
+	}
+	return r.Resp.Items, nil
 }
